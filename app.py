5435fa0e24c18fca1e0afb6d063bbfcb64d3d0de
carlosjpacheco <55545708+carlosjpacheco@users.noreply.github.com>
2020-11-23 13:29:31 -0400

    Validacion Numero de Sandwich

--- app.py
+++ app.py
@@ -1,27 +1,7 @@
-from functions import *
+from functions import main
 
 ##############################################################################
 #########################       MAIN        ##################################
 ##############################################################################
 
-def main():
-    logo = """******************************************
-    *          SANDWICHES UCAB               *
-    ******************************************"""
-    print(logo)
-    sandwiches_comprados = list()
-    valor = str(input('\n Numero de sandwiches que desea ordenar: '))
-    validar =valor.isdigit()
-    if not validar:
-        print('Ingrese nuevamente el numero de sandwiches sin letras\n'
-              'El numero de Sandwiches no puede ser mayor a 10\n\n')
-        main()
-    else:
-        nro_sandwiches = int(valor)
-        total = 0.0
-        for i in range(1,nro_sandwiches + 1):
-           total += opciones(i,sandwiches_comprados)
-
-        verificacion_total(nro_sandwiches,sandwiches_comprados,total)
-
-main()
+main()  
--- functions.py
+++ functions.py
@@ -6,6 +6,31 @@ import sys
 #######################       FUNCTIONS        ##################################
 #################################################################################
 
+def main():
+    logo = """******************************************
+    *          SANDWICHES UCAB               *
+    ******************************************"""
+    print(logo)
+    sandwiches_comprados = list()
+    valor = str(input('\n Numero de sandwiches que desea ordenar: '))
+    validar =valor.isdigit()
+    if not validar:
+        print('***Ingrese nuevamente el numero de sandwiches sin letras***\n')
+        time.sleep(2)
+        clear()
+        main()
+    elif validar or int(valor)>10:
+        print('***El numero de Sandwiches no puede ser mayor a 10***\n\n')
+        time.sleep(2)
+        clear()
+        main()
+    else:
+        nro_sandwiches = int(valor)
+        total = 0.0
+        for i in range(1,nro_sandwiches + 1):
+            total += opciones(i,sandwiches_comprados)
+
+        verificacion_total(nro_sandwiches,sandwiches_comprados,total)
 def clear():
     if name == "nt":
         system("cls")
